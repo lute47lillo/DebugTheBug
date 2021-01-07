ff835f92884cc08adf26a14f5f0f4fa68f0caef7
lute47lillo <lutelillocs47@gmail.com>
2021-01-06 20:33:16 -0800

    Switch of state:    - Change to Space Shooter Arcade setup    - Add sprite features like movement    - Eliminated Tiled Map

--- src/MyGame.py
+++ src/MyGame.py
@@ -2,6 +2,7 @@
 Starting Template
 """
 import arcade
+import math
 
 from src.RimboPlayer import RimboPlayer
 
@@ -11,6 +12,7 @@ SCREEN_TITLE = "Rimbo Rogue"
 CHARACTER_SCALING = 0.5
 TILE_SCALING = 0.5
 GRAVITY = 1
+
 
 class MyGame(arcade.Window):
     """
@@ -23,25 +25,22 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        #Create the player
+        # Create the player
         player1 = RimboPlayer()
         self.player1 = player1
 
-        #Create a list of enemies that interact or not with the player.
+        # Create a list of enemies that interact or not with the player.
         self.listEnemies = arcade.SpriteList(None)
 
-        #Created the basic engine for the movements.
+        # Created the basic engine for the movements.
         self.physicsP1 = None
 
-
-        #arcade.set_background_color(arcade.color.AMAZON)
-
+        # arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-
         # Create your sprites and sprite lists here
-        #Create a map
-        map_name = ":resources:tmx_maps/initialMapRR.tmx"
+        # Create a map
+        '''map_name = ":resources:tmx_maps/initialMapRR.tmx"
         platforms_layer_name = 'BackGround'
         my_map = arcade.tilemap.read_tmx(map_name)
         self.wall_list = arcade.tilemap.process_layer(map_object=my_map,
@@ -50,11 +49,10 @@ class MyGame(arcade.Window):
                                                       use_spatial_hash=True)
 
         if my_map.background_color:
-            arcade.set_background_color(my_map.background_color)
+            arcade.set_background_color(my_map.background_color)'''
 
         # Created the basic engine for the movements.
-        self.physicsP1 = arcade.PhysicsEngineSimple(self.player1.player_sprite, self.wall_list)
-
+        self.physicsP1 = arcade.PhysicsEngineSimple(self.player1.player_sprite, self.listEnemies)
 
     def on_draw(self):
         """
@@ -68,7 +66,7 @@ class MyGame(arcade.Window):
         # Call draw() on all your sprite lists below
         self.player1.player_list.draw()
         self.listEnemies.draw()
-        self.wall_list.draw()
+        # self.wall_list.draw()
 
     def on_update(self, delta_time):
         """
@@ -76,8 +74,9 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        #Update the basic engine when keys are pressed.
+        # Update the basic engine when keys are pressed.
         self.physicsP1.update()
+
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -92,7 +91,6 @@ class MyGame(arcade.Window):
         """
         Called whenever the user lets off a previously pressed key.
         """
-
         self.player1.onKeyReleased(key)
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
@@ -119,7 +117,6 @@ def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
-
 
 
 if __name__ == "__main__":
--- src/RimboPlayer.py
+++ src/RimboPlayer.py
@@ -1,55 +1,80 @@
 import arcade
+import math
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
-CHARACTER_SCALING = 3
+CHARACTER_SCALING = 0.5
 PLAYER_MOVEMENT_SPEED = 5
+SPEED_ANGLE = 5
 
 
 class RimboPlayer:
 
     def __init__(self):
-        """self.health = health
-        self.xPos = xPos
-        self.yPos = yPos"""
 
         self.player_sprite = None
         self.player_list = None
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.speed = 0
+        self.angle = 0
+        self.change_angle = 0
+        self.max_speed = 4
+        self.thrust = 0
         self.startSprite()
 
-    #Create Sprite of the player.
+    # Create Sprite of the player.
     def startSprite(self):
         self.player_list = arcade.SpriteList()
-        image_source = ":resources:images/createdSprites/Rimbona.png"
+        image_source = ":resources:images/space_shooter/playerShip1_green.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
+        self.player_sprite.center_x = 300
+        self.player_sprite.center_y = 500
         self.player_list.append(self.player_sprite)
 
     def onKeyPressed(self, key):
-        #Create an action for different movements
 
+        # Create an action for different movements
         if key == arcade.key.W:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.S:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            self.player_sprite.speed = PLAYER_MOVEMENT_SPEED
 
-        #Update it and add the condition when both are pressed and handle it.
+        elif key == arcade.key.S:
+            self.player_sprite.speed = -PLAYER_MOVEMENT_SPEED
+
+        elif key == arcade.key.D:
+            self.player_sprite.change_angle = -SPEED_ANGLE
+
+        elif key == arcade.key.A:
+            self.player_sprite.change_angle = SPEED_ANGLE
+
+        self.update()
+
+        # Update it and add the condition when both are pressed and handle it.
 
     def onKeyReleased(self, key):
 
-        if key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.A:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.D:
-            self.player_sprite.change_x = 0
+        if key == arcade.key.W or key == arcade.key.S:
+            self.player_sprite.speed = 0
+
+        elif key == arcade.key.A or key == arcade.key.D:
+            self.player_sprite.change_angle = 0
+
+        self.update()
+
+    def update(self):
+        # Rotate the ship
+        self.player_sprite.angle += self.player_sprite.change_angle
+
+        self.player_sprite.change_x = -math.sin(math.radians(self.player_sprite.angle)) * self.player_sprite.speed
+
+        self.player_sprite.change_y = math.cos(math.radians(self.player_sprite.angle)) * self.player_sprite.speed
+
+        self.player_sprite.center_x += self.player_sprite.change_x
+
+        self.player_sprite.center_y += self.player_sprite.change_y
+
+
 
 
 
